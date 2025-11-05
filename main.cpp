c5b2565eba9a5e87f319ce3cbe107e43885ca8e9
Jasmine <jazzyysuarez@gmail.com>
2025-11-04 23:02:13 -0800

    update

--- main.cpp
+++ main.cpp
@@ -11,7 +11,16 @@
 using namespace std;
 
 // Define a function to simulate one time period (hour) of changes
+    // Parameters: map of gates, number of intervals
 
 int main() {
-    return  0;
+    // Initialize a map to store gate info, each associated with an array of lists
+    // for arrivals, departures, and delays
+
+    // Open an external file to read initial flight data and populate the map
+        // If file doesn't open, print error and exit
+
+    // Read data from file and populate map
+
+    return 0;
 }
